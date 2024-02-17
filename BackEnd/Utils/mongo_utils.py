142314d2fcd8d9980a5d4f6b7adb422b1c2e5efd
Devarshi Goswami <devarshigoswami@Devarshis-MacBook-Air.local>
2024-02-17 16:19:15 +0000

    added user data handler

---
+++ BackEnd/Utils/mongo_utils.py
@@ -0,0 +1,25 @@
+import os
+import motor.motor_asyncio
+
+class MongoUtils:
+    def __init__(self):
+        self.cached_db = None
+
+    def connect_to_database(self):
+        if self.cached_db:
+            return self.cached_db
+
+        # Fetch MongoDB URI from environment variable
+        mongodb_uri = os.environ.get('MONGO_URI')
+
+        if not mongodb_uri:
+            raise ValueError("MongoDB URI not found in environment variables.")
+
+        # Connect to MongoDB using the fetched URI
+        client = motor.motor_asyncio.AsyncIOMotorClient(mongodb_uri)
+
+        # Specify which database we want to use (you can customize this based on your needs)
+        db = client.LendEase
+
+        self.cached_db = db
+        return db
---
+++ BackEnd/main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
+from Utils.mongo_utils import MongoUtils
+from Classes.user_data_handler import UserDataHandler
+
+app = FastAPI()
+handler = Mangum(app)
+
+# Enable CORS (Cross-Origin Resource Sharing) for all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+mongo_instance = MongoUtils()
+mongo = mongo_instance.connect_to_database()
+
+
+@app.post("/login", response_model=dict)
+async def login_user(username: str, password: str):
+    user_handler = UserDataHandler(username=username, password=password, email="")
+    return await user_handler.authenticate_user(mongo)
+
+
+@app.post("/logout", response_model=dict)
+async def logout_user(username: str, token: str):
+    user_handler = UserDataHandler(username=username, password="", email="")
+    return await user_handler.logout_user(mongo, token)
+
+
+@app.post("/putUserData", response_model=dict)
+async def put_user_data(username: str, email: str, password: str, image_data: bytes):
+    user_handler = UserDataHandler(username=username, password=password, email=email)
+    return await user_handler.put_user_data(mongo, image_data)
+
+
+@app.get("/getUserData", response_model=dict)
+async def get_user_data(username: str):
+    try:
+        user_handler = UserDataHandler(username=username, password="", email="")
+        return await user_handler.get_user_data(mongo)
+    except HTTPException as e:
+        # If it's an HTTPException, return a detailed error response
+        return {"statusCode": e.status_code, "error": str(e.detail)}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8090)
